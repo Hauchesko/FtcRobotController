e05c51cf72ae00ab10fe8d1695caf24c7fa419ba
Hauchesko <assilabdurakhman@gmail.com>
2025-11-10 23:03:34 +0500

    Variable
    Variable

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VariablePractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VariablePractice.java
@@ -1,3 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class VariablePractice extends OpMode {
+    @Override
+    public void init() {
+        int TeamNumber = 23014;
+        double MotorSpeed = 0.75;
+        boolean ClawClosed = true;
+
+        telemetry.addData("Team Number", TeamNumber);
+    }
 
     @Override
     public void loop() {
